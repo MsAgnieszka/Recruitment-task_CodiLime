0c6a877564b8f0f5e087ac3c2724c8b3f1184c4b
Agnieszka Uzarek <agnieszka.czerebak@gmail.com>
2022-10-04 14:18:51 +0200

    Custom eslint config and added additional JSX rules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+function App () {
   return (
     <h1>
       Recruitment task - CodiLime
     </h1>
-  );
+  )
 }
 
-export default App;
+export default App
